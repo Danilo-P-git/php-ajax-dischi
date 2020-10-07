95e49e6483c0fe202c9d1ec3f4e111c7d0c0d611
Danilo Patanè <d.patane98@gmail.com>
2020-10-07 16:24:11 +0200

    Structure done

--- index.php
+++ index.php
@@ -9,11 +9,12 @@
    <head>
      <meta charset="utf-8">
      <title></title>
+     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="dist/app.css">
    </head>
-   <body>
+   <body  class="color-first">
      <header>
        <div class="container-full color-second">
         <div class="container-half">
@@ -25,7 +26,34 @@
        </div>
      </header>
      <main>
-
+      <div class="container-half">
+        <div class="square-container">
+          <div class="square color-inside">
+            <img src="" alt="IMG">
+            <h4>TITOLO</h4>
+            <p>AUTORE</p>
+            <p><small>anno</small> </p>
+          </div>
+          <div class="square color-inside">
+            <img src="" alt="IMG">
+            <h4>TITOLO</h4>
+            <p>AUTORE</p>
+            <p><small>anno</small> </p>
+          </div>
+          <div class="square color-inside">
+            <img src="" alt="IMG">
+            <h4>TITOLO</h4>
+            <p>AUTORE</p>
+            <p><small>anno</small> </p>
+          </div>
+          <div class="square color-inside">
+            <img src="" alt="IMG">
+            <h4>TITOLO</h4>
+            <p>AUTORE</p>
+            <p><small>anno</small> </p>
+          </div>
+        </div>
+      </div>
      </main>
 
 
